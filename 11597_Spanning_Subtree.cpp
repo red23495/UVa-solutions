b474a8950716e319db72d030db9f46c38efc026b
red23495 <red23495@gmail.com>
2017-11-02 14:28:02 +0600

    Create 11597_Spanning_Subtree.cpp

---
+++ 11597_Spanning_Subtree.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+using ull = unsigned long long;
+
+int main() {
+	int n;
+	int test = 1;
+	while (cin >> n && n) {
+		cout << "Case " << test++ << ": " <<(n >> 1) << endl;
+	}
+	return 0;
+}
